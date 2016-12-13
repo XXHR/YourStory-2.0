63ce8dc57c9630fd1ba1c2398c5daa99f6574821
natasha-t <nthapli@gmail.com>
2016-12-13 14:29:40 -0800

    [add] setup server routes files and database schema/config files

---
+++ db/schema.js
@@ -0,0 +1,13 @@
+// establish postgres connection
+
+const pg = require('pg')
+const Sequelize = require('sequelize')
+
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/yourstory', {
+  dialect: 'postgres',
+  define: {
+    timestamps: false
+  }
+})
+
+module.exports = db

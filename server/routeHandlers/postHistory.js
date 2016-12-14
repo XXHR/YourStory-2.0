612ffe78ec336d0f323016a8fc4df6f79d506259
natasha-t <nthapli@gmail.com>
2016-12-14 15:44:37 -0800

    [add] add pseudocode for post history, test endpoint in postman

--- server/routeHandlers/postHistory.js
+++ server/routeHandlers/postHistory.js
@@ -1,1 +1,12 @@
-postHistory.js
+const User = require('../../db/schema').User;
+const Domain = require('../../db/schema').Domain;
+
+module.exports = (req, res) => {
+
+  // parse through user's chrome history in req.body
+    // extract unique domains list
+        // for each unique domain, save in database for today 
+
+  res.sendStatus(200);
+
+}
--- server/routeHelpers.js
+++ server/routeHelpers.js
@@ -1,4 +1,6 @@
 const postUser = require('./routeHandlers/postUser');
+const postHistory = require('./routeHandlers/postHistory');
+
 
 const db = require('../db/config');
 
@@ -10,4 +12,6 @@ db.authenticate().then(() => {
 
 module.exports = {
   postUser: postUser,
+
+  postHistory: postHistory,
 }

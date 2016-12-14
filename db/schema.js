680e02bdd1200e433f72cda6230235f49e88aa85
natasha-t <nthapli@gmail.com>
2016-12-14 11:28:42 -0800

    [refactor] rearrange query logic for adding domain for today

--- db/schema.js
+++ db/schema.js
@@ -68,44 +68,56 @@ db
   .sync({ force: false })
   .then(() => {
 
-    // User.findOrCreate({ where: { username: 'Natasha' } })
-    //     .spread((user, created) => {
-    //       if(user) {
-    //         Domain.findOrCreate({ where: { domain: 'goodbye.com' } })
-    //               .spread((domain, created) => {
-    //                 console.log(domain.get({
-    //                   plain: true
-    //                 }))
-    //                 user.addDomain(domain, { count: 100, date_added: new Date() });
-    //               })
-    //       }
-    //     })
-
-
-
+    
     let today = new Date();
     let year = today.getFullYear().toString();
     let month = (today.getMonth() + 1).toString();
-    let day = today.getDate().toString();
+    let day = (today.getDate() - 1).toString();
 
     let date = year + '-' + month + '-' + day;
 
-    User.findOne({ where: { username: 'Natasha' } })
-        .then((user) => {
-          user.getDomains()
-              .then((domains) => {
-                console.log('hulu date added', domains[0].dataValues.users_domains.dataValues.date_added);
 
-                console.log('javascript date: ', date, ' sequelize date: ', domains[0].dataValues.users_domains.dataValues.date_added)
+    User.findOrCreate({ where: { username: 'Natasha' } })
+        .spread((user, created) => {
+          if(user) {
+            Domain.findOrCreate({ where: { domain: 'hello.com' } })
+                  .spread((domain, created) => {
+                    console.log(domain.get({
+                      plain: true
+                    }))
 
-                if(date === domains[0].dataValues.users_domains.dataValues.date_added) {
-                  console.log('added today');
-                } else {
-                  console.log('not a match')
-                }
+                    // get user's domains for today
+                    user.getDomains({ where: { date_added:  } })
+                        .then((domains) => {
+                          console.log(typeof domains);
+                        })
+                      // check if new domain is already in list for user for today's date
+                        // if yes, update the count
+                        // if no, add domain to list for today 
+                            // user.addDomain(domain, { count: 100, date_added: new Date() });
+                  })
+          }
+        });
 
-              })
-        })
+
+
+
+    // User.findOne({ where: { username: 'Natasha' } })
+    //     .then((user) => {
+    //       user.getDomains()
+    //           .then((domains) => {
+    //             console.log('domain date added', domains[2].dataValues.users_domains.dataValues.date_added);
+
+    //             console.log('javascript date: ', date, ' sequelize date: ', domains[0].dataValues.users_domains.dataValues.date_added)
+
+    //             if(date === domains[0].dataValues.users_domains.dataValues.date_added) {
+    //               console.log('added today');
+    //             } else {
+                  
+    //             }
+
+    //           })
+    //     })
     
 
 
@@ -121,5 +133,5 @@ module.exports = {
   User: User,
   Domain: Domain,
   Category: Category,
-  UserDomain: UserDomain
+  UserDomain: UserDomain,
 }

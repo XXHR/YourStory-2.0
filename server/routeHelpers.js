7210159af4f5c92c1fe538c182bab39f7fa0e481
natasha-t <nthapli@gmail.com>
2016-12-13 16:30:22 -0800

    [add] set up db schema, include test queries, set up postUser route

--- server/routeHelpers.js
+++ server/routeHelpers.js
@@ -1,11 +1,13 @@
-//require in routes functions from routes directory
+const routes = require('./routes');
+
+const db = require('../db/config');
+
+db.authenticate().then(() => {
+  console.log('Connection established');
+}).catch((err) => {
+  console.log('Unable to connect: ', err);
+});
 
 module.exports = {
-
-
-  postUser: helpers.postUser,
-
-  postHistory: helpers.postHistory
-
-
+  postUser: routes.postUser
 }

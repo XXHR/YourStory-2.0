8fe530793b3c78200f2e2469bfa77b4f91cc2373
melbee <melba@transferhero.com>
2016-12-20 13:04:44 -0800

    [test] added all routehandler starter test specs

--- spec/server spec/spec.js
+++ spec/server spec/spec.js
@@ -1,70 +1,28 @@
-// 'use strict';
+// example spec that connects to db
 
-// const request = require('request');
-// const base_url = 'http://localhost:3000';
-// // const base_url = 'http://yourstory-app.herokuapp.com';
+'use strict';
 
-// const db = require('./test-schema');
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+const db = require('./sample-schema');
 
-// describe('YourStory App', function () {
-//   var foo = 0;
+describe('example database query test', function () {
+  beforeEach(function (done) {
+    db.db
+     .authenticate()
+     .then(function (err) {
+       console.log('Connection established from Jasmine spec.js');
+       done();
+     })
+     .catch(function (err) {
+       console.error.bind(console, 'Connection error from Jasmine spec.js ');
+     });
+  });
 
-//   beforeEach(function () {
-//     foo += 1;
-//     this.db = db.db
-//      .authenticate()
-//      .then(function (err) {
-//        console.log('Connection established jasmine');
-//        done();
-//      })
-//      .catch(function (err) {
-//        console.error.bind(console, 'Sequelize connection error: ');
-//      });
-//   });
-
-//   afterEach(function() {
-//     foo = 0;
-//   });
-
-//   describe('create user', function() {
-//     it('adds a user to user table', function (done) {
-//       expect(
-//         db.User.findOrCreate({ where: { username: 'Melba' } })
-//         ).toBe('Melba');
-//       done();
-//     });
-//   });
-// });
-
-
-
-
-
-
-// 'use strict';
-
-// const Sequelize = require('sequelize');
-// const db = require('./sample-schema');
-
-// describe('database queries', function () {
-//   beforeEach(function (done) {
-//     db.db
-//      .authenticate()
-//      .then(function (err) {
-//        console.log('Connection established from Jasmine spec.js');
-//        done();
-//      })
-//      .catch(function (err) {
-//        console.error.bind(console, 'Connection error from Jasmine spec.js ');
-//      });
-//   });
-
-//   it('adds a user to user table', function (done) {
-//     db.User
-//     .findOrCreate({ where: { username: 'Adele' } }).then((user) => {
-//       expect(user[0].dataValues.username).toBe('Adele');
-//       done();
-//     });
-//   });
-// });
+  it('adds a user to user table', function (done) {
+    db.User
+    .findOrCreate({ where: { username: 'Adele' } }).then((user) => {
+      expect(user[0].dataValues.username).toBe('Adele');
+      done();
+    });
+  });
+});

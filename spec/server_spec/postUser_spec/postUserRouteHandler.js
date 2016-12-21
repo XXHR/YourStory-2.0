bfc9f0373fc8f199b0bf8e3790ee1fe5b116d55b
melbee <melba@transferhero.com>
2016-12-21 13:03:44 -0800

    [fix] renamed files

---
+++ spec/server_spec/postUser_spec/postUserRouteHandler.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const request = require('request');
+const base_url = 'http://localhost:3000';
+// const base_url = 'http://yourstory-app.herokuapp.com';
+
+const db = require('../sample-schema');
+const Sequelize = require('sequelize');
+
+describe('postUser routehandler', function () {
+  describe('GET /', function () {
+    it('returns status 200', function (done) {
+      request.get(base_url, function (error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('GET /api/users', function () {
+    it('returns status 200', function (done) {
+      request.get(base_url + '/api/users', function (error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });    
+  });
+});
+

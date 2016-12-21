8addf066f01811b2945311c447bd4977c5193343
melbee <melba@transferhero.com>
2016-12-21 13:03:44 -0800

    [fix] renamed files

---
+++ spec/server_spec/getCatData_spec/getCatDataRouteHandlerSpec.js
@@ -0,0 +1,28 @@
+// 'use strict';
+
+// const request = require('request');
+// const base_url = 'http://localhost:3000';
+// // const base_url = 'http://yourstory-app.herokuapp.com';
+
+// const db = require('../sample-schema');
+// const Sequelize = require('sequelize');
+
+// describe('getCatData routehandler', function () {
+//   describe('GET /', function () {
+//     it('returns status 200', function (done) {
+//       request.get(base_url, function (error, response, body) {
+//         expect(response.statusCode).toBe(200);
+//         done();
+//       });
+//     });
+//   });
+
+//   describe('GET /api/catData', function () {
+//     it('returns status 200', function (done) {
+//       request.get(base_url + '/api/catData', function (error, response, body) {
+//         expect(response.statusCode).toBe(200);
+//         done();
+//       });
+//     });
+//   });
+// });

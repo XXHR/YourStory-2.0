87ec655ccac8f13041f9eeaed1f5e84890e64028
natasha-t <nthapli@gmail.com>
2016-12-21 12:06:53 -0800

    [fix] change res.status

--- server/routeHandlers/postHistory.js
+++ server/routeHandlers/postHistory.js
@@ -89,19 +89,22 @@ module.exports.postHistory = (req, res) => {
                       });
 
                       promisedAddDomainsToday
-                        .then(() => {
-                          
+                        .then(() => {                         
                           user.getDomains()
                             .then((domains) => {
                               let visData = {};
                               for (let domain of domains) {
                                 if (visData[domain.dataValues.domain]) {
-                                  visData[domain.dataValues.domain] += domain.dataValues.users_domains.count
+                                  visData[domain.dataValues.domain] += domain.dataValues.users_domains.count;
                                 } else {
-                                  visData[domain.dataValues.domain] = domain.dataValues.users_domains.count
+                                  visData[domain.dataValues.domain] = domain.dataValues.users_domains.count;
                                 }
                               }
-                              res.sendStatus(200).json(visData);
+                              console.log('vis data', visData);
+                              res.status(200).json(visData);
+                            })
+                            .catch((err) => {
+                              console.log('error fetching user domains', err);
                             })
                         })
                   })

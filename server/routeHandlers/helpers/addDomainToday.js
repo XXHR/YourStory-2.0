0b37715a7fb0f2282eda8943c0bcb3c2846eb06c
natasha-t <nthapli@gmail.com>
2017-01-04 13:02:24 -0800

    [refactor] convert saving of domains to user domains table into promise all funciton

--- server/routeHandlers/helpers/addDomainToday.js
+++ server/routeHandlers/helpers/addDomainToday.js
@@ -1,23 +1,44 @@
 const UserDomain = require('../../../db/schema').UserDomain;
 
-const addDomainToday = (domainIds, userId, date) => {
-  for (let domain of domainIds) {
 
-     UserDomain.findAll({ where: { userId, domainId: domain.id, date_added: date } })
-      .then((userDomain) => {
-        // if no domains saved for today's date, add domain to table
-         if (userDomain.length === 0) {
-          UserDomain.create({ count: domain.totalCount, date_added: date, domainId: domain.id, userId: userId })
-                    .catch((error) => {
-                      console.log('unable to save in users domains table', error);
-                    });
-          } else {
-            let currentCount = userDomain[0].dataValues.count;
-            userDomain[0].update({ count: currentCount + domain.totalCount });
-          }    
-      });
-    }
+const readOrWriteDomainForUser = (userId, domainId, totalCount, date) => {    
+  UserDomain.findAll({ where: { userId, domainId, date_added: date } })
+    .then((userDomain) => {
+      // if no domains saved for today's date, add domain to table
+       if (userDomain.length === 0) {
+        UserDomain.create({ count: totalCount, date_added: date, domainId, userId })
+                  .catch((error) => {
+                    console.log('unable to save in users domains table', error);
+                  });
+        } else {
+          let currentCount = userDomain[0].dataValues.count;
+          userDomain[0].update({ count: currentCount + totalCount })
+          .catch((error) => {
+              console.log('unable to update in users domains table', error);
+          });
+        }    
+    });
+ };
+
+const addDomainToday = (domainObjsArray, userId, date) => {
+  // map over domainObjsArray
+  // create promises for each item
+
+  const promisedDomainObjsArray = domainObjsArray.map((domainObj) => {
+    return new Promise((resolve, reject) => {
+          return resolve(readOrWriteDomainForUser(userId, domainObj.id, domainObj.totalCount, date))
+        })
+  })
+
+
+  const userDomains = Promise.all(promisedDomainObjsArray).then((values) => {
+    console.log(values);
+  })
+
+  return userDomains;
+
 };
 
 
 module.exports = addDomainToday;
+

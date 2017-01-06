203d0e0a11318f1d02d108c4c7843bf71fb7ffe4
natasha-t <nthapli@gmail.com>
2017-01-05 16:40:57 -0800

    [refactor] complete refactored modularized postHistory route

--- server/routeHandlers/helpers/getDateToday.js
+++ server/routeHandlers/helpers/getDateToday.js
@@ -8,4 +8,3 @@ const getDateToday = () => {
 };
 
 module.exports = getDateToday;
-
--- server/routeHandlers/helpers/postHistoryData.js
+++ server/routeHandlers/helpers/postHistoryData.js
@@ -20,12 +20,6 @@ const postHistoryData = (allHistory, chromeID) => {
     .then(() => {
       return getVisData(chromeID);
     });
-<<<<<<< HEAD
-
 };
 
 module.exports = postHistoryData;
-
-=======
-};
->>>>>>> [add] add addDomainsForToday helper function and getVisData

af50be88b73e677161a3547d0ee534633a352a93
natasha-t <nthapli@gmail.com>
2017-01-09 16:05:00 -0800

    [add] assign category to newly created domains

--- server/routeHandlers/helpers/bulkCreateNewDomains.js
+++ server/routeHandlers/helpers/bulkCreateNewDomains.js
@@ -1,4 +1,8 @@
 const Domain = require('../../../db/schema').Domain;
+const Category = require('../../../db/schema').Category;
+
+const axios = require('axios');
+const btoa = require('btoa');
 
 const bulkCreateNewDomains = (uniqueDomains) => {
    return Domain.findAll({ where: { domain: Object.keys(uniqueDomains) } })
@@ -23,6 +27,37 @@ const bulkCreateNewDomains = (uniqueDomains) => {
       });
 
       return Domain.bulkCreate(domainsBulkCreate);
+
+    })
+    .then((domains) => {
+      for (let domain of domains) {
+        console.log('DOMAIN', domain.dataValues.domain)
+        const apiUrl = 'https://api.webshrinker.com/categories/v2/';
+        const hashURL = btoa('http://www.' + domain.dataValues.domain);
+        axios({
+          method: 'get',
+          url: apiUrl + hashURL,
+          auth: {
+            username: 'UL1QVH3FAtR6eoEJJIs4',
+            password: 'ZCZCYLA6wtqYNDpxbbRE',
+          },
+        })
+        .then((response) => {
+          return Category.findOrCreate({ where: { category: response.data.data[0].categories[0] } });
+        })
+        .catch((err) => {
+          console.log('error getting category from webshrinker', err);
+        })
+        .then((cat) => {
+          Domain.findOne({ where: { domain: domain.dataValues.domain } })
+          .then((domain) => {
+            return domain.update({ categoryId: cat[0].dataValues.id });
+          })
+        })
+        .catch((err) => {
+          console.log('error finding or creating category', err);
+        });
+      }
     });
 };
 

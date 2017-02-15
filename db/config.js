44859b472ab7402ae7e712658689ea94caa05d2c
melbee <melba@transferhero.com>
2017-02-15 11:51:36 -0800

    [cleanup] removed old code from config

--- db/config.js
+++ db/config.js
@@ -1,19 +1,3 @@
-// 'use strict';
-
-// //  instantiate new sequelize database
-// const pg = require('pg');
-// const Sequelize = require('sequelize');
-
-// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/testyourstory', {
-//   dialect: 'postgres',
-//   define: {
-//     timestamps: false,
-//   },
-//   timezone: 'America/Los_Angeles',
-// });
-
-// module.exports = db;
-
 const pg = require('pg');
 const Sequelize = require('sequelize');
 
@@ -28,21 +12,3 @@ const db = new Sequelize(process.env.PRODUCTION_DB_NAME || 'testyourstory', proc
 });
 
 module.exports = db;
-
-
-
-
-// const pg = require('pg');
-// const Sequelize = require('sequelize');
-
-// const db = new Sequelize('postgres', 'postgres', 'flyingcolors', {
-//   host: 'localhost',
-//   port: 5432,
-//   dialect: 'postgres',
-//   define: {
-//     timestamps: false,
-//   },
-//   timezone: 'America/Los_Angeles',
-// });
-
-// module.exports = db;
